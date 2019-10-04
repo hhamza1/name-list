c238b7402cbb653dce683a4011fcf0be9451ef87
unknown <hamzahmitou@gmail.com>
2019-10-04 12:47:53 +0300

    Dynamic Table

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-let table = document.querySelector('table');
-let data = ['Bron','Allen','John'];
+let tbody = document.querySelector('tbody');
+let data = ['Bron','Allen','John','Woody','Jayjay','Kiki'];
 
-console.log();
+console.log('Data: ', data);
+console.log('Table rows: ', tbody.rows.length);
 
-for(let i=0; i < data.length; i++) 
-{
-    table.rows[i+1].cells[0].innerHTML = data.indexOf(data[i]);
-    table.rows[i+1].cells[1].innerHTML = data[i];
+
+for(let i=tbody.rows.length; i<data.length; i++) {
+    let newRow = tbody.insertRow(-1);
+    newRow.setAttribute('draggable',true); 
+    let newCell0 = newRow.insertCell(-1);   
+    let newCell1 = newRow.insertCell(0);
+}
+
+for(let i=0; i < data.length; i++) {
+    tbody.rows[i].cells[0].innerHTML = data.indexOf(data[i]);
+    tbody.rows[i].cells[1].innerHTML = data[i];
 }

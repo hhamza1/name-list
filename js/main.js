7c24ce58c666e248a39bbc879b008f8b24bcc448
unknown <hamzahmitou@gmail.com>
2019-10-05 17:35:40 +0300

    Added the sorting method

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 let tbody = document.querySelector('tbody');
+let body = document.querySelector('body');
 let form = document.querySelector('form');
 let submit = document.querySelector('#submit');
-let data = [];
+let localData = [];
+const sortBtn = document.querySelector('#sort');
+
+
+
+const  cellsNumber =  2;
+
 
 const newEntry = () => {
-    for(let i=tbody.rows.length; i<data.length; i++) {
+    for(let i=tbody.rows.length; i<localData.length; i++) {
         let newRow = tbody.insertRow(-1);
         newRow.setAttribute('draggable',true); 
         let newCell0 = newRow.insertCell(-1);   
@@ -12,34 +19,58 @@ const newEntry = () => {
     }
 }
 
-const displayData = () => {
-    for(let i=0; i < data.length; i++) {
-        tbody.rows[i].cells[0].innerHTML = data.indexOf(data[i]) + 1;
-        tbody.rows[i].cells[1].innerHTML = data[i];
+const displayData = (local) => {
+    console.log(local);
+    for(let i=0; i < local.length; i++) {
+            tbody.rows[i].cells[0].innerHTML = local.indexOf(local[i]) + 1;
+            tbody.rows[i].cells[1].innerHTML = local[i];
     }
 
 }
 
 const dataToArray = (el) => {
-    data.push(el);
+    localData.push(el);
+    localStorage.clear();
+    localStorage.setItem("localData", JSON.stringify(localData));
     console.log(el);
 }
 
+
+/*============= Sorting method ==============*/
+sortBtn.addEventListener('click', () => {
+    console.log('triggered');
+    let newLocalData = localData.sort();
+    localData = newLocalData;
+    displayData(localData);
+});
+
+
 submit.addEventListener('click', (event)=> {
     event.preventDefault();
-    let dataValue = document.querySelector("input").value;
-    if(dataValue) {
-        dataToArray(dataValue);
+    let inputValue = document.querySelector("input").value;
+    if(inputValue) {
+        dataToArray(inputValue);
     }
     newEntry();
-    displayData();
-    console.log(data);
-    localStorage.setItem("data", JSON.stringify(data));
-})
+    displayData(localData);
+    console.log(localData);
+    console.log(localStorage);
+
+});
 
 
+/* window.addEventListener('onload', () => {
+    const newlocalData = JSON.parse(localStorage.getItem("localData")) ;
+    console.log('New localData: ',newlocalData);
+    console.log('localData before assignment: ',localData);
+    localData = newlocalData;
+    console.log('localData after assignment: ',localData);
+    for(let i=0; i < localData.length; i++){
+        newEntry();
+        displayData(localData);
+    }
 
 
+});
 
-
-
+console.log('Out of the event listener: ',localData); */

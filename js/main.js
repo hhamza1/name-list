3eef300da703c9b9c36791a57570c845185fa474
unknown <hamzahmitou@gmail.com>
2019-10-07 17:10:52 +0300

    Case insensitve sorting

--- js/main.js
+++ js/main.js
@@ -10,60 +10,75 @@ const reverse = document.querySelector('#reverse');
 
 
 const newEntry = () => {
-    for(let i=tbody.rows.length; i<localData.length; i++) {
-        let newRow = tbody.insertRow(-1);
-        newRow.setAttribute('draggable',true); 
-        let newCell0 = newRow.insertCell(-1);   
-        let newCell1 = newRow.insertCell(0);
+    if(localData){
+        for(let i=tbody.rows.length; i<localData.length; i++) {
+            let newRow = tbody.insertRow(-1);
+            newRow.setAttribute('draggable',true); 
+            let newCell0 = newRow.insertCell(-1);   
+            let newCell1 = newRow.insertCell(0);
+        }
     }
+    
 }
 
-const displayData = (local) => {
-    for(let i=0; i < local.length; i++) {
-            tbody.rows[i].cells[0].innerHTML = local.indexOf(local[i]) + 1;
-            tbody.rows[i].cells[1].innerHTML = local[i];
-            tbody.rows[i].setAttribute('id',  local.indexOf(local[i]) + 1);
+const displayData = () => {  //add checking if null
+    for(let i=0; i < localData.length; i++) {
+            tbody.rows[i].cells[0].innerHTML = localData.indexOf(localData[i]) + 1;
+            tbody.rows[i].cells[1].innerHTML = localData[i];
+            tbody.rows[i].setAttribute('id',  localData.indexOf(localData[i])+ 1);
     }
 
 }
 
 const dataToArray = (el) => {
-    localData.push(el);
-    previousState.push(el);
-    localStorage.clear();
-    localStorage.setItem("localData", JSON.stringify(localData));
-    localStorage.setItem("previousState", JSON.stringify(previousState));
-    }
+        localData.push(el);
+        previousState.push(el);
+        localStorage.clear();
+        localStorage.setItem("localData", JSON.stringify(localData));
+        localStorage.setItem("previousState", JSON.stringify(previousState));
+}
+
+
 
 
 /*============= Sorting method ==============*/
 sortBtn.addEventListener('click', () => {
-    let newLocalData = localData.sort();
+    let newLocalData = localData.sort((a,b) => { 
+        /*
+            Higher Order function to allow Case insensitive sorting
+        */
+        let x = a.toLowerCase();
+        let y = b.toLowerCase();
+
+        if(x < y) {
+            return -1;
+        }
+        if(x < y) {
+            return 1;
+        }
+        
+        return 0;
+    });
     localData = newLocalData;
-    displayData(localData);
+    displayData();
     localStorage.setItem("localData", JSON.stringify(localData));
 
 });
 
-/*============= Loading the localStorage method ==============*/
-window.addEventListener('load', () => {
-    localData = JSON.parse(localStorage.getItem("localData"));
-    previousState = JSON.parse(localStorage.getItem("localData"));
-    newEntry();
-    displayData(localData);
-});
-
-console.log('Local Data', localData);
-
 /*===============Reverse to previous State=================*/
 
 reverse.addEventListener('click', () => {
-    displayData(previousState);
+    localData = previousState;
+    displayData();
 });
 
-
-
-
+/*============= Loading the localStorage method ==============*/
+window.addEventListener('load', () => {
+        localData = JSON.parse(localStorage.getItem("localData")) || [];
+        previousState = JSON.parse(localStorage.getItem("previousState")) || [];
+        newEntry();
+        displayData();
+});
 
 /*===============Label Submission=================*/
 
@@ -72,10 +87,12 @@ submit.addEventListener('click', (event)=> {
     event.preventDefault();
     let inputValue = document.querySelector("input").value;
     if(inputValue) {
-        dataToArray(inputValue);
+        if(localData) {
+            dataToArray(inputValue);
+        }
     }
     newEntry();
-    displayData(localData);
+    displayData();
 });
 
 

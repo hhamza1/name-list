c39a7584d54bd9b0533b6920cfde32b048b1da6c
unknown <hamzahmitou@gmail.com>
2019-10-07 09:46:17 +0300

    Added the loading event listener

--- js/main.js
+++ js/main.js
@@ -13,10 +13,8 @@ const newEntry = () => {
     for(let i=tbody.rows.length; i<localData.length; i++) {
         let newRow = tbody.insertRow(-1);
         newRow.setAttribute('draggable',true); 
-        newRow.setAttribute('class','dropzone'); 
         let newCell0 = newRow.insertCell(-1);   
         let newCell1 = newRow.insertCell(0);
-        newCell0.setAttribute('class','drag'); 
     }
 }
 
@@ -25,6 +23,7 @@ const displayData = (local) => {
     for(let i=0; i < local.length; i++) {
             tbody.rows[i].cells[0].innerHTML = local.indexOf(local[i]) + 1;
             tbody.rows[i].cells[1].innerHTML = local[i];
+            tbody.rows[i].setAttribute('id',  local.indexOf(local[i]) + 1);
     }
 
 }
@@ -41,12 +40,24 @@ const dataToArray = (el) => {
 
 /*============= Sorting method ==============*/
 sortBtn.addEventListener('click', () => {
-    console.log('triggered');
     let newLocalData = localData.sort();
-    console.log(previousState);
     localData = newLocalData;
     displayData(localData);
+    localStorage.setItem("localData", JSON.stringify(localData));
+
 });
+
+/*============= Loading the localStorage method ==============*/
+window.addEventListener('load', () => {
+    let loadedData = JSON.parse(localStorage.getItem("localData"));
+    console.log(loadedData);
+    localData = loadedData;
+    console.log(localData);
+    newEntry();
+    displayData(localData);
+});
+
+
 
 /*===============Reverse to previous State=================*/
 
@@ -73,65 +84,4 @@ submit.addEventListener('click', (event)=> {
 
 
 /*================Drag&Drop=======================*/
-let dragged ;
 
-document.addEventListener('drag', (event) => { // trigger the drag event
-
-}, false);
-
-document.addEventListener('dragstart', (event) => {
-    dragged = event.target;
-    event.target.style.opacity = .5;
-}, false);
-
-document.addEventListener('dragend', (event) => {
-    event.target.style.opacity = 1;
-}, false);
-
-document.addEventListener('dragover', (event) => {
-    event.preventDefault();
-}, false);
-
-document.addEventListener("dragenter", function(event) {
-    // highlight potential drop target when the draggable element enters it
-    if (event.target.parentElement.className == "dropzone") {
-      event.target.style.background = "#666";
-    }
-  
-  }, false);
-
-  document.addEventListener("dragleave", function(event) {
-    // reset background of potential drop target when the draggable element leaves it
-    if (event.target.parentElement.className == "dropzone") {
-      event.target.style.background = "";
-    }
-  
-  }, false);
-
-  document.addEventListener("drop", function(event) {
-    // prevent default action (open as link for some elements)
-    event.preventDefault();
-    // move dragged elem to the selected drop target
-    if (event.target.parentElement.className== "dropzone") {
-      event.target.style.background = "";
-      dragged.parentNode.removeChild( dragged );
-      event.target.parentElement.appendChild( dragged );
-    }
-  }, false);
-
-
-/* window.addEventListener('onload', () => {
-    const newlocalData = JSON.parse(localStorage.getItem("localData")) ;
-    console.log('New localData: ',newlocalData);
-    console.log('localData before assignment: ',localData);
-    localData = newlocalData;
-    console.log('localData after assignment: ',localData);
-    for(let i=0; i < localData.length; i++){
-        newEntry();
-        displayData(localData);
-    }
-
-
-});
-
-console.log('Out of the event listener: ',localData); */
